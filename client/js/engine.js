4104f166cf9380a6e028d00620b237a3e749b5b1
Ikuo Terado <eqobar@gmail.com>
2013-06-28 18:38:50 +0900

    Added buttons and handlers

--- client/js/engine.js
+++ client/js/engine.js
@@ -1,5 +1,34 @@
 
+BlockEvent = {
+	LEFT: 0,
+	RIGHT: 1,
+	DOWN: 2,
+	ROTATE: 3
+};
+
 function dummy() {
 	console.log('dummy()');
 }
 
+function moveBlock(blockEvent) {
+	switch (blockEvent) {
+		case BlockEvent.LEFT:
+			console.log('left');
+			break;
+
+		case BlockEvent.RIGHT:
+			console.log('right');
+			break;
+
+		case BlockEvent.DOWN:
+			console.log('down');
+			break;
+
+		case BlockEvent.ROTATE:
+			console.log('rotate');
+			break;
+
+		default:
+			break;
+	}
+}
--- client/js/main.js
+++ client/js/main.js
@@ -8,6 +8,35 @@
 
 	function initialize() {
 		console.log('initialize()');
+
+		$('#left').click(onButtonPress);
+		$('#right').click(onButtonPress);
+		$('#down').click(onButtonPress);
+		$('#rotate').click(onButtonPress);
+	}
+
+	function onButtonPress (event) {
+		var blockEvent = event.target.id;
+		switch (blockEvent) {
+			case 'left':
+				moveBlock(BlockEvent.LEFT);
+				break;
+
+			case 'right':
+				moveBlock(BlockEvent.RIGHT);
+				break;
+
+			case 'down':
+				moveBlock(BlockEvent.DOWN);
+				break;
+
+			case 'rotate':
+				moveBlock(BlockEvent.ROTATE);
+				break;
+
+			default:
+				break;
+		}
 	}
 
 	function mainLoop() {
