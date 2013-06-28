d8375e0dccb9121c6df3dcabd000f818a78528fd
Ikuo Terado <eqobar@gmail.com>
2013-06-28 17:15:41 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+(function() {
+
+	$(document).ready(function($) {
+		initialize();
+		mainLoop();
+	});
+
+	function initialize() {
+		console.log('initialize()');
+	}
+
+	function mainLoop() {
+		dummy();
+	}
+
+})();
