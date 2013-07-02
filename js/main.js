1b50a85deb24fd262e1acb3c3041182d2684890e
Ikuo Terado <eqobar@gmail.com>
2013-07-02 11:10:40 +0900

    Added comments

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 
 (function() {
 
+	// Map from DOM id to block event
 	var BUTTON_MAP = {
-		'left': BlockEvent.LEFT,
-		'right': BlockEvent.RIGHT,
-		'down': BlockEvent.DOWN,
+		'left':   BlockEvent.LEFT,
+		'right':  BlockEvent.RIGHT,
+		'down':   BlockEvent.DOWN,
 		'rotate': BlockEvent.ROTATE,
-		'add': BlockEvent.ADD
+		'add':    BlockEvent.ADD
 	};
 
+	// Map from key code to block event
 	var KEY_MAP = {
-		37: BlockEvent.LEFT,
-		39: BlockEvent.RIGHT,
-		40: BlockEvent.DOWN,
-		38: BlockEvent.ROTATE,
-		32: BlockEvent.ROTATE,
-		13: BlockEvent.ADD
+		37: BlockEvent.LEFT,	// Left key
+		39: BlockEvent.RIGHT,	// Right key
+		40: BlockEvent.DOWN,	// Down key
+		38: BlockEvent.ROTATE,	// Up key
+		32: BlockEvent.ROTATE,	// Space key
+		13: BlockEvent.ADD		// Enter key
 	};
 
 	$(document).ready(function($) {
@@ -23,24 +25,30 @@
 		mainLoop();
 	});
 
+	// Initialize
 	function initialize() {
+		// Set an event listener for buttons to be pressed
 		$('.blockEvent').click(onButtonPress);
 
+		// Set an event listener for keys to be pressed
 		$(document).keydown(onKeyPress);
 	}
 
+	// Handle button press event
 	function onButtonPress (event) {
 		var code = event.target.id;
 		var blockEvent = BUTTON_MAP[code];
 		executeBlockEvent(blockEvent);
 	}
 
+	// Handle key press event
 	function onKeyPress (event)  {
 		var code = event.keyCode;
 		var blockEvent = KEY_MAP[code];
 		executeBlockEvent(blockEvent);
 	}
 
+	// Execute block event like adding and moving block
 	function executeBlockEvent (blockEvent) {
 		if (blockEvent !== undefined) {
 			if (blockEvent === BlockEvent.ADD) {
