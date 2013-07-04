23dcf34b09fa65fe593b00342221c39497443b14
Ikuo Terado <eqobar@gmail.com>
2013-07-05 01:18:47 +0900

    Changed the path from server to client

--- server/tetris/tetriss.js
+++ server/tetris/tetriss.js
@@ -24,7 +24,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../../client')));
 
 // development only
 if ('development' == app.get('env')) {
